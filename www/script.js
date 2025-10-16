0aedf0dc3f75e8e5773996955c74c628f0cb53cb
novilaa <159593281+novilaa@users.noreply.github.com>
2025-10-16 22:00:48 +0200

    Delete www directory

--- www/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("subscriptionForm").addEventListener("submit", function(e) {
-    const username = document.querySelector("[name='username']").value;
-    const journal = document.querySelector("[name='journal']").value;
-    alert(`Вы оформили подписку: ${username}, журнал "${journal}"`);
-});
